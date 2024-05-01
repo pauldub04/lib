bf06dc4b0879813f7effa424ae569ac025cf882f
Pavel Dubnyakov <49102209+pauldub04@users.noreply.github.com>
2024-05-01 14:05:55 +0300

    Update li_chao.cpp

--- data structures/li_chao.cpp
+++ data structures/li_chao.cpp
@@ -1,3 +1,4 @@
+// https://codeforces.com/problemset/problem/319/C
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -8,25 +9,23 @@ typedef vector< vll > vll2;
 typedef pair<ll, ll> pll;
 typedef string str;
 
-//mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
-//#define rand(l, r) uniform_int_distribution<ll>(l, r)(rng)
+// mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+// #define rand(l, r) uniform_int_distribution<ll>(l, r)(rng)
 
 #define IOS ios::sync_with_stdio(false); cin.tie(nullptr)
-#define vin(a) {for (auto& _ : (a)) {cin >> _;}}
-#define vout(a) {for (auto _ : (a)) {cout << _ << " ";} cout << "\n";}
-#define forn(i, n) for (int (i) = 0; (i) < int(n); (i)++)
+#define vin(x_points) {for (auto& _ : (x_points)) {cin >> _;}}
+#define vout(x_points) {for (auto _ : (x_points)) {cout << _ << " ";} cout << "\n";}
 #define all(x) (x).begin(), (x).end()
-#define sz(a) ((int) ((a).size()))
+#define sz(x_points) int((x_points).size())
 
 #define F first
 #define S second
 #define pb push_back
 
-const int inf = 1e9, mod = 1e9+7, N = 2e6+5, LG = 30;
+const int inf = 1e9, mod = 1e9+7, N = 1e5+10, LG = 30;
 const ll INF = 1e18;
 const ld eps = 1e-9;
 
-const int diff = 1e6;
 
 struct Line {
     ll k;
@@ -35,54 +34,59 @@ struct Line {
         return k*x + b;
     }
 };
-
+ 
 // li chao
-vector<Line> t;
-ll n;
-vll a, b;
-
-void add(Line cur, ll x=0, ll lx=0, ll rx=n) {
+vector<Line> tree;
+vll x_points;
+ 
+void add_line(Line cur, ll x=0, ll lx=0, ll rx=sz(x_points)) {
     ll m = (lx+rx)/2;
-    bool left = cur.get(a[lx]) < t[x].get(a[lx]);
-    bool mid = cur.get(a[m]) < t[x].get(a[m]);
-    if (mid)
-        swap(t[x], cur);
-
-    if (rx-lx == 1)
-        return;
-    if (left == mid)
-        add(cur, 2*x+2, m, rx);
-    else
-        add(cur, 2*x+1, lx, m);
-}
-
-ll get(ll i, ll x=0, ll lx=0, ll rx=n) {
-    if (rx-lx == 1)
-        return t[x].get(a[i]);
-
-    ll m = (lx+rx)/2;
-    if (i < m)
-        return min(t[x].get(a[i]), get(i, 2*x+1, lx, m));
-    else
-        return min(t[x].get(a[i]), get(i, 2*x+2, m, rx));
-}
-
-
-int solve(ll tc) {
-    cin >> n;
-
-    t.resize(4*n, {0, INF});
-    a.resize(n); vin(a);
-    b.resize(n); vin(b);
-
-    vll dp(n);
-    add({b[0], 0});
-
-    for (int i = 1; i < n; i++) {
-        dp[i] = get(i);
-        add({b[i], dp[i]});
+    bool left = cur.get(x_points[lx]) < tree[x].get(x_points[lx]);
+    bool mid = cur.get(x_points[m]) < tree[x].get(x_points[m]);
+    if (mid) {
+        swap(tree[x], cur);
     }
 
+    if (rx-lx == 1) {
+        return;
+    }
+    if (left == mid) {
+        add_line(cur, 2*x+2, m, rx);
+    } else {
+        add_line(cur, 2*x+1, lx, m);
+    }
+}
+ 
+ll get_ans(ll i, ll x=0, ll lx=0, ll rx=sz(x_points)) {
+    if (rx-lx == 1) {
+        return tree[x].get(x_points[i]);
+    }
+ 
+    ll m = (lx+rx)/2;
+    if (i < m) {
+        return min(tree[x].get(x_points[i]), get_ans(i, 2*x+1, lx, m));
+    } else {
+        return min(tree[x].get(x_points[i]), get_ans(i, 2*x+2, m, rx));
+    }
+}
+
+
+int solve() {
+    ll n; cin >> n;
+    x_points.resize(n); vin(x_points);
+    vll b(n); vin(b);
+ 
+    tree.resize(4*n, {0, INF});
+
+    vll dp(n);
+    dp[0] = 0;
+    add_line({b[0], dp[0]});
+ 
+    for (int i = 1; i < n; i++) {
+        dp[i] = get_ans(i);
+        add_line({b[i], dp[i]});
+    }
+    
     cout << dp.back();
 
     return 0;
@@ -90,11 +94,13 @@ int solve(ll tc) {
 
 int main() {
     IOS;
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
 
     ll tc = 1;
-//    cin >> tc;
-    forn(i, tc) {
-        solve(i);
+    // cin >> tc;
+    while (tc--) {
+        solve();
     }
 
     return 0;
