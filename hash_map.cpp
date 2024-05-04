ad44a188a5c178022f650a6829339d210d649836
Pavel Dubnyakov <49102209+pauldub04@users.noreply.github.com>
2024-05-05 01:01:59 +0300

    Update hash_map.cpp

--- hash_map.cpp
+++ hash_map.cpp
@@ -7,12 +7,13 @@ struct chash { // large odd number for C
 	const uint64_t C = ll(4e18 * acos(0)) | 71;
 	ll operator()(ll x) const { return __builtin_bswap64(x*C); }
 };
-__gnu_pbds::cc_hash_table<ll, ll, chash> cnt;
 __gnu_pbds::gp_hash_table<ll, ll, chash> cnt;
+__gnu_pbds::cc_hash_table<ll, ll, chash> cnt;
+
 
 // без хеша
-__gnu_pbds::cc_hash_table<ll, ll> cnt;
 __gnu_pbds::gp_hash_table<ll, ll> cnt;
+__gnu_pbds::cc_hash_table<ll, ll> cnt;
 
 
 // https://codeforces.com/blog/entry/72675?locale=ru
